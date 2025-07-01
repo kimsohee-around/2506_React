b6686b07cab66fc7351d72b242fca069cfe76e51
kimsohee <koreait.sec2020@gmail.com>
2025-07-01 17:51:57 +0900

    day02 (input과 useState)

--- lesson02/src/components/Calculator.jsx
+++ lesson02/src/components/Calculator.jsx
@@ -7,6 +7,7 @@ export default function Calculator() {
   // 문자열을 수치 형식으로 변환 후 사칙연산. Number(first) / Number(second)
   const [error, setError] = useState('') // 오류메시지 저장 상태값
 
+  // 입력값의 유효성 검사 함수
   function validation(value) {
     console.log(/^-?\d+(\.\d+)?$/.test(value), value) // 정규식.test(값)
     // 정규식 통과할때만 setFirst 로 상태값 변경.
@@ -32,19 +33,28 @@ export default function Calculator() {
     setResult(Number(first) * Number(second))
   }
   function handleDiv() {
-    setResult(Number(first) / Number(second))
+    // second 가 0일 때는 Infinity => 유효성검사 필요
+    // first 가 0 일 때는 0
+    if (Number(second) !== 0 && validation(first) && validation(second)) {
+      setResult(Number(first) / Number(second))
+      setError('')
+    } else if (Number(second) === 0) {
+      setError('0으로 나눌수 없습니다.')
+    }
   }
 
   return (
     <div className='container'>
       Calculator
       <h3>계산기</h3>
+      <div style={{ color: 'red', fontSize: '0.8rem' }}>{error}</div>
       <input
         type='number'
         placeholder='첫번째 값을 입력하세요.'
         value={first}
         onChange={(e) => setFirst(e.target.value)}
         // onChange={(e) => handleNumberInput(e.target.value)}
+        // 입력중에는 정규식 정확한 검사 불가
       />
       <button onClick={handleAdd}>+</button>
       <button onClick={handleSub}>-</button>
